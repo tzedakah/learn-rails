7f5e895767b22b6bd6454a19d67bb1caa430ae7c
Antonio Chinea <creative.studio.acts@gmail.com>
2013-11-30 17:22:01 -0500

    Create mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,15 +1,32 @@
 class VisitorsController < ApplicationController
 
-def new
+  def new
   # Rails.logger.debug 'DEBUG: entering new method'
-  @owner = Owner.new
+  # @owner = Owner.new
   # Rails.logger.debug 'DEBUG: Owner name is  '+ @owner.name
   # raise 'Deliberate Failure'
   # DISASTER
   # render 'visitors/new'
   # flash.now[:notice] = 'Welcome!'
   # flash.now[:alert] = 'My birthday is soon.'
-end
+    @visitor = Visitor.new
+  end
 
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def secure_params
+    params.require(:visitor).permit(:email)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 LearnRails::Application.routes.draw do
   # post 'contact', to: 'contacts#process_form'
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
