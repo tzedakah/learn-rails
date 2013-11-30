3d373f8fd61ea7eae28b08a2a78e0426ca0030eb
Antonio Chinea <creative.studio.acts@gmail.com>
2013-11-30 10:40:54 -0500

    Create contact form, Rails-Way

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearnRails::Application.routes.draw do
+  # post 'contact', to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
